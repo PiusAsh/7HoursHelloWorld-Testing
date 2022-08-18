09717cdd41cef9cfee133dff1ef2d23c8494326b
PiusAsh <Pius1ash@gmail.com>
2022-08-18 17:20:03 +0100

    Add project files.

---
+++ 7HoursHelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _7HoursHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Times(4, 5));
+            //Console.WriteLine(Divide(11, 2));
+        }
+
+        //public static int Times(int num1, int num2)
+        //{
+        //    return num1 * num2;
+        //}
+
+        //public static double Divide(double num1, double num2)
+        //{
+        //    return num1 / num2;
+        //}
+    }
+}
